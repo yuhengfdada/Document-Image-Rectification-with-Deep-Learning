0590038c412893c6da07f576a08c023c5bae5a1d
lxb007981 <lxb007981@hotmail.com>
2020-08-16 10:35:47 +0800

    update .gitignore

--- fineName.py
+++ fineName.py
@@ -1,8 +1,8 @@
-from pathlib import Path
+'''from pathlib import Path
 p = Path('.')
 li = list(p.glob('*.png'))
 f = open('test.txt','w')
 for i, file in enumerate(li):
     f.write(file.stem)
     f.write('\n')
-f.close()
+f.close()'''
